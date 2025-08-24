c6a31b3d75ede2f6d7d027db3b3ab0c2a80800b0
Kieron Wray <kieron.wray@gmail.com>
2025-08-24 13:08:41 +0100

    added working k6 tests and grafana

---
+++ ApplicationUnderTest/SimpleApi1/Program.cs
@@ -0,0 +1,72 @@
+using OpenTelemetry.Metrics;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var serviceName = builder.Environment.ApplicationName;
+
+builder.Services.AddOpenTelemetry()
+      .ConfigureResource(resource => resource.AddService(serviceName))
+
+      .WithTracing(tracing => tracing
+          .AddAspNetCoreInstrumentation()
+
+          .AddConsoleExporter())
+      .WithMetrics(metrics => metrics
+          .AddAspNetCoreInstrumentation()
+          .AddPrometheusExporter()
+          .AddHttpClientInstrumentation()
+            .AddEventCountersInstrumentation(c =>
+            {
+                c.AddEventSources(
+                        "Microsoft.AspNetCore.Hosting",
+                        "Microsoft-AspNetCore-Server-Kestrel",
+                        "System.Net.Http",
+                        "System.Net.Sockets");
+            })
+            .AddMeter("Microsoft.AspNetCore.Hosting", "Microsoft.AspNetCore.Server.Kestrel")
+          .AddConsoleExporter());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+
+// Configure the Prometheus scraping endpoint
+app.MapPrometheusScrapingEndpoint();
+
+var summaries = new[]
+{
+    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+};
+
+app.MapGet("/weatherforecast", () =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return forecast;
+})
+.WithName("GetWeatherForecast");
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
